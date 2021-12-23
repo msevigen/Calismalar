fc073a8e04ee00e2b062f330cd06641c1dadd2a8
mhmtsvgn <msevigenm@gmail.com>
2021-12-23 22:27:37 +0100

    2

---
+++ src/Replit/For_while.java
@@ -0,0 +1,26 @@
+package Replit;
+
+public class For_while {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 1'den 100'e kadar olan doğal sayıların toplamını bulan programı yazınız.
+		 * 
+		 * OutPut:
+		 * 
+		 * Sayilarin Toplami : 5050
+		 */
+
+		int num = 0;
+
+		for (int i = 0; i <= 100; i++) {
+
+			num += i;
+
+		}
+		System.out.println(num);
+
+	}
+
+}
---
+++ src/Replit/For_while_StringTekrarEdenleriDondurme.java
@@ -0,0 +1,27 @@
+package Replit;
+
+public class For_while_StringTekrarEdenleriDondurme {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Bir String icerisinde yinelenen karakterleri döndüren bir kod
+		 * yazıniz.(mülakat Sorusu)
+		 * 
+		 * Input : String str = “Javaisalsoeasy”
+		 * 
+		 * Output: a s
+		 */
+
+		String str = "Javaisalsoeasy";
+
+		for (int i = 0; i < str.charAt(i); i++) {
+
+			
+		}
+		
+			System.out.println(str);
+
+	}
+
+}
